db355ef73f1905724e76c071f2d794bb6cc2a632
Enrique Laguna <58639436+EnriqueLaguna@users.noreply.github.com>
2021-03-01 07:30:19 -0600

    Initial commit

---
+++ config/passport.js
@@ -0,0 +1,67 @@
+const passport = require ( 'passport' );
+const GoogleStrategy = require ( 'passport-google-oauth20' ).Strategy;
+const pool = require('../database/pool');
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: 'http://localhost:3001/auth/google/callback' ,
+        },
+        function (accessToken, refreshToken, profile, done) {
+            //console.log(profile);
+            done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser(async function (user, done) {
+    console.log(user);
+    const idReducido = parseInt(Math.sqrt(Math.sqrt(user.id)));
+
+    //Revisar si el ID ya esta en la base de datos
+
+    (await pool).query('select * from users where id=$1', [idReducido], async(qerr, qres) => {
+        if(qerr){
+            console.log(qerr);
+            return;
+        }
+        const existUser = qres.rows[0];
+
+//Si no existe un user con ese id, se inserta en la base de datos
+
+        if(!existUser){
+            (await pool).query('insert into users("id", "fullname", "email","imagen") values($1, $2, $3, $4)', [idReducido, user.displayName, user.emails[0].value, user.photos[0].value], 
+            (qerr2, qres2) => {
+                if(qerr2){
+                    console.log(qerr2);
+                    return;
+                }
+            });
+        }
+    });
+
+    done(null, idReducido);
+});
+
+passport.deserializeUser(async function (id, done) {
+    if(id >= 1000000000000000000000){
+        id = parseInt(Math.sqrt(Math.sqrt(id)));
+    }
+    console.log(id);
+    (await pool).query('select u.id, u.fullname, u.email, u.imagen from users u where u.id = $1', [id], async(qerr, qres) => {
+        if(!qerr){
+            const user=qres.rows[0];
+            if(user){
+                done(null,user);
+            }
+            else{
+                done();
+            }
+        }else{
+            console.error(qerr);
+        } 
+        
+    });
+});
---
+++ index.js
@@ -0,0 +1,68 @@
+const createError = require('http-errors');
+const passport=require('passport');
+const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const lessMiddleware = require('less-middleware');
+const logger = require('morgan');
+const cookieSession=require('cookie-session');
+const hbs = require('express-handlebars');
+
+require('dotenv').config();
+require('./config/passport');
+
+const authRouter = require('./routes/auth');
+
+const app = express();
+
+// view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  maxAge:24 * 60 * 60 * 1000,
+  keys:['clave']
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRouter);
+
+app.get('/',(req,res)=>{
+    res.render('home',{template:req.user?'logout':'login'})
+});
+
+app.get('/profile',(req,res)=>{
+    console.log(req.user);
+  const profile = req.user;
+  const imagen = profile.imagen;
+  const name = profile.fullname;
+  const email = profile.email;
+  //console.log("profairu",profile);
+  res.render('profile',{name:name,imagen:imagen,email:email});
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// // error handler
+app.use(function(err, req, res, next) {
+  req.logout();
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3001, () => {
+    console.log('escuchando en el puerto 3001');
+})
